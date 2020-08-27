a9fa2378edc3731d78be075cbf8834828affbee6
tunabel <tuan.23a5@gmail.com>
2020-08-27 11:02:39 +0700

    Initial commit

---
+++ src/main/java/com/github/tunabel/gateway/SbmsBreweryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.github.tunabel.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsBreweryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsBreweryGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tunabel/gateway/SbmsBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tunabel.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmsBreweryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
